24eb3353d79ac0d5e1c35cb3d3f6784820fdc12b
lanyun1103 <4737557@qq.com>
2023-12-02 19:45:05 +0800

    chore: clean settings.py format and add Chinese option

--- marker/settings.py
+++ marker/settings.py
@@ -11,10 +11,10 @@ import torch
 class Settings(BaseSettings):
     # General
     TORCH_DEVICE: str = "cpu"
-    INFERENCE_RAM: int = 40 # How much VRAM each GPU has (in GB).
-    VRAM_PER_TASK: float = 2.5 # How much VRAM to allocate per task (in GB).  Peak marker VRAM usage is around 3GB, but avg across workers is lower.
-    DEBUG: bool = False # Enable debug logging
-    DEFAULT_LANG: str = "English" # Default language we assume files to be in, should be one of the keys in TESSERACT_LANGUAGES
+    INFERENCE_RAM: int = 40  # How much VRAM each GPU has (in GB).
+    VRAM_PER_TASK: float = 2.5  # How much VRAM to allocate per task (in GB).  Peak marker VRAM usage is around 3GB, but avg across workers is lower.
+    DEBUG: bool = False  # Enable debug logging
+    DEFAULT_LANG: str = "Chinese"  # Default language we assume files to be in, should be one of the keys in TESSERACT_LANGUAGES
 
     SUPPORTED_FILETYPES: Dict = {
         "application/pdf": "pdf",
@@ -38,8 +38,9 @@ class Settings(BaseSettings):
         "French": "fra",
         "German": "deu",
         "Russian": "rus",
+        "Chinese": "chi_sim",
     }
-    TESSERACT_TIMEOUT: int = 20 # When to give up on OCR
+    TESSERACT_TIMEOUT: int = 20  # When to give up on OCR
     SPELLCHECK_LANGUAGES: Dict = {
         "English": "en",
         "Spanish": "es",
@@ -48,18 +49,20 @@ class Settings(BaseSettings):
         "German": "de",
         "Russian": "ru",
     }
-    OCR_ALL_PAGES: bool = False # Run OCR on every page even if text can be extracted
-    OCR_PARALLEL_WORKERS: int = 2 # How many CPU workers to use for OCR
-    OCR_ENGINE: str = "ocrmypdf" # Which OCR engine to use, either "tesseract" or "ocrmypdf".  Ocrmypdf is higher quality, but slower.
+    OCR_ALL_PAGES: bool = False  # Run OCR on every page even if text can be extracted
+    OCR_PARALLEL_WORKERS: int = 2  # How many CPU workers to use for OCR
+    OCR_ENGINE: str = "ocrmypdf"  # Which OCR engine to use, either "tesseract" or "ocrmypdf".  Ocrmypdf is higher quality, but slower.
 
     # Nougat model
-    NOUGAT_MODEL_MAX: int = 512 # Max inference length for nougat
-    NOUGAT_TOKEN_BUFFER: int = 256 # Number of tokens to buffer above max for nougat
-    NOUGAT_HALLUCINATION_WORDS: List[str] = ["[MISSING_PAGE_POST]", "## References\n", "**Figure Captions**\n", "Footnote",
-                                  "\par\par\par", "## Chapter", "Fig.", "particle", "[REPEATS]", "[TRUNCATED]", "### ", "effective field strength", "\Phi_{\rm eff}"]
-    NOUGAT_DPI: int = 96 # DPI to render images at, matches default settings for nougat
-    NOUGAT_MODEL_NAME: str = "0.1.0-small" # Name of the model to use
-    NOUGAT_BATCH_SIZE: int = 6 if TORCH_DEVICE == "cuda" else 1 # Batch size for nougat, don't batch on cpu
+    NOUGAT_MODEL_MAX: int = 512  # Max inference length for nougat
+    NOUGAT_TOKEN_BUFFER: int = 256  # Number of tokens to buffer above max for nougat
+    NOUGAT_HALLUCINATION_WORDS: List[str] = ["[MISSING_PAGE_POST]", "## References\n", "**Figure Captions**\n",
+                                             "Footnote",
+                                             "\par\par\par", "## Chapter", "Fig.", "particle", "[REPEATS]",
+                                             "[TRUNCATED]", "### ", "effective field strength", "\Phi_{\rm eff}"]
+    NOUGAT_DPI: int = 96  # DPI to render images at, matches default settings for nougat
+    NOUGAT_MODEL_NAME: str = "0.1.0-small"  # Name of the model to use
+    NOUGAT_BATCH_SIZE: int = 6 if TORCH_DEVICE == "cuda" else 1  # Batch size for nougat, don't batch on cpu
 
     # Layout model
     BAD_SPAN_TYPES: List[str] = ["Caption", "Footnote", "Page-footer", "Page-header", "Picture"]
@@ -67,23 +70,23 @@ class Settings(BaseSettings):
     LAYOUT_CHUNK_OVERLAP: int = 64
     LAYOUT_DPI: int = 96
     LAYOUT_MODEL_NAME: str = "vikp/layout_segmenter"
-    LAYOUT_BATCH_SIZE: int = 8 # Max 512 tokens means high batch size
+    LAYOUT_BATCH_SIZE: int = 8  # Max 512 tokens means high batch size
 
     # Ordering model
-    ORDERER_BATCH_SIZE: int = 32 # This can be high, because max token count is 128
+    ORDERER_BATCH_SIZE: int = 32  # This can be high, because max token count is 128
     ORDERER_MODEL_NAME: str = "vikp/column_detector"
 
     # Final editing model
     EDITOR_BATCH_SIZE: int = 4
     EDITOR_MAX_LENGTH: int = 1024
     EDITOR_MODEL_NAME: str = "vikp/pdf_postprocessor_t5"
-    ENABLE_EDITOR_MODEL: bool = False # The editor model can create false positives
-    EDITOR_CUTOFF_THRESH: float = 0.9 # Ignore predictions below this probability
+    ENABLE_EDITOR_MODEL: bool = False  # The editor model can create false positives
+    EDITOR_CUTOFF_THRESH: float = 0.9  # Ignore predictions below this probability
 
     # Ray
-    RAY_CACHE_PATH: Optional[str] = None # Where to save ray cache
+    RAY_CACHE_PATH: Optional[str] = None  # Where to save ray cache
     RAY_DASHBOARD_HOST: str = "127.0.0.1"
-    RAY_CORES_PER_WORKER: int = 1 # How many cpu cores to allocate per worker
+    RAY_CORES_PER_WORKER: int = 1  # How many cpu cores to allocate per worker
 
     @computed_field
     @property
